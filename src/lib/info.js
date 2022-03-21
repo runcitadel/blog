c0ea0f25d7c7aaa9a9389e3e8ae7ad11fcb148ee
Aaron Dewes <aaron.dewes@protonmail.com>
2022-03-21 10:44:55 +0100

    Update info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -5,9 +5,9 @@
 /**
  * Used for the header of the page and in SEO
  */
-export const name = 'SvelteKit Blog Template' // replace with your name
+export const name = 'Citadel blog' // replace with your name
 
 /**
  * Used for SEO in /posts/[slug]
  */
-export const website = 'https://yourdomain.com'
+export const website = 'https://blog.runcitadel.space'
